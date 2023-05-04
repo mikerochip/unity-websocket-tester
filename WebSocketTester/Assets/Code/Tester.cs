2ac5804f9eee4a5f43185af2033c5a2952da6aa9
Mike Schweitzer <mschweitzer@gmail.com>
2023-05-04 00:27:37 -0700

    Basic scene and code setup

--- WebSocketTester/Assets/Code/Tester.cs
+++ WebSocketTester/Assets/Code/Tester.cs
@@ -1,21 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using Mikerochip.WebSocket;
 
 namespace WebSocketTester
 {
     public class Tester : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        public TMP_InputField _Server;
+        public Button _ConnectButton;
+        public Button _DisconnectButton;
+        public TMP_Text _State;
+        public TMP_InputField _OutgoingMessage;
+        public Button _SendButton;
+        public TMP_Text _IncomingMessage;
+        public WebSocketConnection _Connection;
 
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+
+            _ConnectButton.onClick.AddListener(() => _Connection.Connect(_Server.text));
+            _DisconnectButton.onClick.AddListener(() => _Connection.Disconnect());
+            _SendButton.onClick.AddListener(() => _Connection.AddOutgoingMessage(_OutgoingMessage.text));
         }
 
-        // Update is called once per frame
-        void Update()
+        private void Update()
         {
+            _State.text = _Connection.State.ToString();
 
+            while (_Connection.TryRemoveIncomingMessage(out string message))
+                _IncomingMessage.text = message;
         }
     }
 }
